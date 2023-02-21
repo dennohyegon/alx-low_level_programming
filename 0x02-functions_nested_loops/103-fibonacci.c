bacbe0d784d52ef4119c42443c72662d94a34459
Dennis Arap Yegon <kipkemoidennisyegon@gmail.com>
2023-02-21 12:59:58 -0800

    Even Liber Abbaci

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+/**
+ *
+ *main-prints the numbers
+ */
+int main(void)
+{
+int current = 2;
+int previous = 1;
+int temp;
+int sum = 2;
+while (current <= 4000000)
+{
+temp = current;
+current += previous;
+previous = temp;
+if (current % 2 == 0) 
+{
+sum += current;
+}
+}
+printf("%d\n", sum);
+return (0);
+}
