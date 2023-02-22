00e12026f46fa236e113a726a40a6ba618ae8d30
Dennis Arap Yegon <kipkemoidennisyegon@gmail.com>
2023-02-22 07:16:54 -0800

    98 Battery Street, the OG

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -6,19 +6,27 @@
 */
 #include <stdio.h>
 
-void print_to_98(int n) {
+void print_to_98(int n)
+{
 int i;
-if (n <= 98) {
-for (i = n; i <= 98; i++) {
+if (n <= 98)
+{
+for (i = n; i <= 98; i++)
+{
 printf("%d", i);
-if (i != 98) {
+if (i != 98)
+{
 printf(", ");
 }
 }
-} else {
-for (i = n; i >= 98; i--) {
+}
+else
+{
+for (i = n; i >= 98; i--)
+{
 printf("%d", i);
-if (i != 98) {
+if (i != 98)
+{
 printf(", ");
 }
 }
