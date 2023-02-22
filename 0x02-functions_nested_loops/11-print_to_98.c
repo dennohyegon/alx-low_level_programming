ed27032abaa5b8251fafbcc58186c78497657e3c
Dennis Arap Yegon <kipkemoidennisyegon@gmail.com>
2023-02-22 07:12:59 -0800

    98 Battery Street, the OG

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -1,46 +1,27 @@
-#include"main.h"
-
+#include <stdio.h>
 /**
- * print_to_98 - print n to 98 counts
- *           separated by comma, followed
- *           by space and number should be
- *           printed in order
- *
- * @n: input
+*print_to_98-prints numbers separated by a comma
+*@n: input
+*
 */
-void print_to_98(int n)
-{
-if (n <= 98)
-{
-for (; n<=98; n++)
-{
-if (n == 98 )
-{
-printf("%d", n)
-printf("n\");
-break;
-}
-else
-{
-printf("%d ", n);
-}
-}
-}
-else
-{
-for (; n >= 98; n--)
-{
-if (n == 98)
-{
-printf("%d",  n);
-printf("\n");
-break;
-}
-else
-{
-printf("%d, ", n);
-}
-}
-}
-}
+#include <stdio.h>
 
+void print_to_98(int n) {
+int i;
+if (n <= 98) {
+for (i = n; i <= 98; i++) {
+printf("%d", i);
+if (i != 98) {
+printf(", ");
+}
+}
+} else {
+for (i = n; i >= 98; i--) {
+printf("%d", i);
+if (i != 98) {
+printf(", ");
+}
+}
+}
+printf("\n");
+}
