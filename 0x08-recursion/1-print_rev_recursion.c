dd1d628c3c937600c8241ccf557da84b4d2c0050
Dennis Arap Yegon <kipkemoidennisyegon@gmail.com>
2023-03-08 10:18:32 -0800

    1-print_rev_recursion.c

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -2,12 +2,13 @@
 /**
 * _puts_recursion - prints a string, followed by a new line.
 * @s: string
-* Return (*s) : recursive input
+*Return: Always 0 (Success)
 */
 void _print_rev_recursion(char *s)
 {
-if (*s == '\0') {
-return;
+if (*s == '\0')
+{
+return(0);
 }
 _print_rev_recursion(s + 1);
 putchar(*s);
