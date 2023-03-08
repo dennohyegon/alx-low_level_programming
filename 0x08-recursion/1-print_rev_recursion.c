a7f9befb799157f1cf5b8820e4cb02d41b0484bc
Dennis Arap Yegon <kipkemoidennisyegon@gmail.com>
2023-03-08 10:13:53 -0800

    1-print_rev_recursion.c

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -1,13 +1,14 @@
 #include <stdio.h>
 /**
- * _puts_recursion - prints a string, followed by a new line.
- * @s: string
- * Return: no return.
- */
-void _print_rev_recursion(char *s) {
-    if (*s == '\0') {
-        return;
-    }
-    _print_rev_recursion(s + 1);
-    putchar(*s);
+* _puts_recursion - prints a string, followed by a new line.
+* @s: string
+* Return (*s) : recursive input
+*/
+void _print_rev_recursion(char *s)
+{
+if (*s == '\0') {
+return;
+}
+_print_rev_recursion(s + 1);
+putchar(*s);
 }
