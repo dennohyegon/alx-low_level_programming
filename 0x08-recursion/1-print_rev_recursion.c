f4fc508b5e00f7b05a8361816cc97d2daa3b6594
Dennis Arap Yegon <kipkemoidennisyegon@gmail.com>
2023-03-08 10:08:08 -0800

    1-print_rev_recursion.c

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -4,13 +4,10 @@
  * @s: string
  * Return: no return.
  */
-void _puts_recursion(char *s)
-{
-	if (*s != '\0')
-	{
-		_putchar(*s);
-		_puts_recursion(s + 1);
-	}
-	else
-		_putchar('\n');
+void _print_rev_recursion(char *s) {
+    if (*s == '\0') {
+        return;
+    }
+    _print_rev_recursion(s + 1);
+    putchar(*s);
 }
